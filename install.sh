6b06d3180c512f8bb29de99f0811c11bda39ab00
grewstad <dogestad24@gmail.com>
2026-04-17 14:55:00 +0530

    install: remount root with 1G size

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ set -e
 
 # fulfill host dependencies
 printf "quay: fulfilling host dependencies...\n"
-mount -o remount,size=2G / || true
+mount -o remount,size=1G / || true
 apk add -q cryptsetup util-linux dosfstools xfsprogs binutils mkinitfs pciutils eudev
 udevadm settle
 
